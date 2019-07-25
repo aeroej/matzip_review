bf5949a6fed9377c32c95d4bf60b3603e3f67df1
eunchae <eunchae@DESKTOP-A9VFHBK>
2019-07-26 02:59:09 +0900

    blog앱의 views수정
    -detail함수에 평균별점 구하는 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Portfolio, Comment
+from django.db.models import Avg
 
 # Create your views here.
 
@@ -8,7 +10,18 @@ from django.shortcuts import render
 #def create(request):
 #def update(request, blog_id):
 #def delete(request, blog_id):
-#def detail(request, blog_id):
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Portfolio, pk=blog_id)
+
+    #평균별점 구하는 부분(참고 : 템플릿상에서 {{average}}값이 평균별점값)
+    a = Comment.objects.filter(post=blog_id).aggregate(Avg('grade'))
+    b = a['grade__avg']
+    if b is None:
+        average = 0
+    else:
+        average = round(b,1) 
+
+    return render(request, 'detail.html', {'blog': blog_detail,"average":average})
 
 
 #def comment(request, post_id):

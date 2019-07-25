b6f9dd60beb3524039e0a6e56dc724d0b59623c1
eunchae <eunchae@DESKTOP-A9VFHBK>
2019-07-25 23:56:49 +0900

    blog앱의 models에서 Comment부분 수정
    -별점을 나타내는 grade필드 추가

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,21 @@ from django.db import models
 # Create your models here.
 
 
-
 #class Portfolio(models.Model):
 
-#class Comment(models.Model):
+class Comment(models.Model):
+    grade_total= (
+    (5, '5점'),
+
+    (4, '4점'),
+
+    (3, '3점'),
+
+    (2, '2점'),
+
+    (1, '1점'),
+    )
+    grade = models.FloatField(
+        choices=grade_total,
+        default= 5
+    )

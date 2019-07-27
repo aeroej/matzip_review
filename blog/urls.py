ac69d1b5ecead12ffa9f5a80e572bb9b925e23ae
dahae0320 <ss7ssy@naver.com>
2019-07-27 16:39:22 +0900

    detail,create완성/url수정/setting한국시수정

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('detail/', views.detail, name='detail'),
+    path('detail/<int:blod_id>/', views.detail, name='detail'),
     path('create/', views.create, name='create'),
-    path('comment/', views.comment, name='comment'),
-    path('update/', views.update, name='update'),
-    path('delete/', views.delete, name='delete'),
-    path('co_update/', views.co_update, name='co_update'),
-    path('co_delete/', views.co_delete, name='co_delete'),
+    path('update/<int:blod_id>/', views.update, name='update'),
+    path('delete/<int:blod_id>/', views.delete, name='delete'),
+    path('comment/<int:post_id>/', views.comment, name='comment'),
+    path('co_update/<int:post_id>/', views.co_update, name='co_update'),
+    path('co_delete/<int:post_id>/', views.co_delete, name='co_delete'),
 ]

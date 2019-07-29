cc58f498eb20d7172bb51cd9c547376630fc9e92
tbs01 <tbs01@LAPTOP-4IKPRLO6>
2019-07-29 13:21:16 +0900

    blog/views import redirect 추가

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Portfolio, Comment
 from .forms import NewBlog, CommentForm # 모듈 추가
 from django.db.models import Avg

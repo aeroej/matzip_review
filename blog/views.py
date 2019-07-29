b7c780c15359a1ba8eaa9b470addcdd99e20be2e
tbs01 <tbs01@LAPTOP-4IKPRLO6>
2019-07-29 13:11:09 +0900

    blog/views 댓글 작성까지 완료, 수정삭제 추가안함

--- blog/views.py
+++ blog/views.py
@@ -54,6 +54,15 @@ def detail(request, blog_id):
     return render(request, 'detail.html', {'blog': blog_detail,"average":average})
 
 
-#def comment(request, post_id):
-#def co_update(request, post_id):
-#def co_delete(request,post_id):
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})

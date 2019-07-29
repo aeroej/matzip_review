4dbd016123e8085c8ae86da731688009ece7e491
dahae0320 <ss7ssy@naver.com>
2019-07-29 14:34:07 +0900

    오타수정 및 comment

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('detail/<int:blod_id>/', views.detail, name='detail'),
+    path('detail/<int:blog_id>/', views.detail, name='detail'),
     path('create/', views.create, name='create'),
-    path('update/<int:blod_id>/', views.update, name='update'),
-    path('delete/<int:blod_id>/', views.delete, name='delete'),
+    path('update/<int:blog_id>/', views.update, name='update'),
+    path('delete/<int:blog_id>/', views.delete, name='delete'),
     path('comment/<int:post_id>/', views.comment, name='comment'),
     path('co_update/<int:post_id>/', views.co_update, name='co_update'),
     path('co_delete/<int:post_id>/', views.co_delete, name='co_delete'),
